b3ca3d63020ae78719918b75d680a5dc4d0e4c74
Aravind SV <arvind.sv@gmail.com>
2016-12-13 17:07:05 -0500

    Use a sleep method that works

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ echo Let us pretend that this is building something ... and takes 20 seconds
 
 FOR /l %%A in (1, 1, 20) DO (
   ECHO Building [[01;31m %%A of 20 [00m]
-  TIMEOUT /T 1 /NOBREAK >NUL
+  PING 192.0.2.1 -n 1 -w 1000 >NUL
 )
 
 (
