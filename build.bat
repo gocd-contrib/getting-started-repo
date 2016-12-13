91bb59e4888c84b2dcc7f72a8fb1abdfb992a145
Aravind SV <arvind.sv@gmail.com>
2016-12-13 16:46:56 -0500

    Add a Windows build script

---
+++ build.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+
+echo Let us pretend that this is building something ... and takes 20 seconds
+
+FOR /l %%A in (1, 1, 20) DO (
+  ECHO Building [[01;31m %%A of 20 [00m]
+  TIMEOUT /T 1 /NOBREAK >NUL
+)
+
+(
+  ECHO ^<html^>
+  ECHO ^<body^>
+  ECHO ^<h3^>An example artifact^</h3^>
+  ECHO ^<pre^>
+  ECHO ==== ==== ====
+  ECHO An example artifact, created on:
+  DATE /T
+  TIME /T
+  ECHO Pipeline which created it: %GO_PIPELINE_NAME%
+  ECHO Pipeline counter was: %GO_PIPELINE_COUNTER%
+  ECHO ==== ==== ====
+  ECHO ^</pre^>
+  ECHO ^<body^>
+  ECHO ^</html^>
+) >my-artifact.html
